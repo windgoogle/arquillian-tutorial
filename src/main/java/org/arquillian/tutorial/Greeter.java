955041acdce6575ddf0f5ee7380e83b5546f7751
windgoogle <windgoogle@aliyun.com>
2017-08-21 18:39:35 +0800

    arguillian tutorial update

---
+++ src/main/java/org/arquillian/tutorial/Greeter.java
@@ -0,0 +1,24 @@
+package org.arquillian.tutorial;
+
+import java.io.PrintStream;
+
+import java.io.PrintStream;
+import javax.inject.Inject;
+
+public class Greeter {
+
+    private PhraseBuilder phraseBuilder;
+
+    @Inject
+    public Greeter(PhraseBuilder phraseBuilder) {
+        this.phraseBuilder = phraseBuilder;
+    }
+
+    public void greet(PrintStream to, String name) {
+        to.println(createGreeting(name));
+    }
+
+    public String createGreeting(String name) {
+        return phraseBuilder.buildPhrase("hello", name);
+    }
+}
